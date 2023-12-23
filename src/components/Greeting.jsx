d07856ee0a0f9cbdac2216bcd8fa0aab49fb5b19
DevVali <90271108+DevVali@users.noreply.github.com>
2023-12-23 21:42:38 +0100

    style: run prettier

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 
 export default function Greeting({ messages }) {
-    const randomMessage = () =>
-        messages[Math.floor(Math.random() * messages.length)];
+	const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
 
-    const [greeting, setGreeting] = useState(randomMessage);
+	const [greeting, setGreeting] = useState(randomMessage);
 
-    return (
-        <>
-            <p onClick={() => setGreeting(randomMessage())}>
-                {greeting}. I'm Vali.
-            </p>
-        </>
-    );
+	return (
+		<>
+			<p onClick={() => setGreeting(randomMessage())}>{greeting}. I'm Vali.</p>
+		</>
+	);
 }
